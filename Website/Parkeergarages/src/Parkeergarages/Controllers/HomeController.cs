7d53faab97d4a6bcd8736302a8dbf63041c8755d
Rinesh Ramadhin <Rinesh.Ramadhin@gmail.com>
2016-03-17 21:58:16 +0100

    Added website
    Modified Website structure for use case
    Website flow
    Added basic Google Maps functionality

---
+++ Website/Parkeergarages/src/Parkeergarages/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+
+namespace Parkeergarages.Controllers
+{
+    public class HomeController : Controller
+    {
+        // 
+        // GET: /Home/Error
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
